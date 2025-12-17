ea177fa4a60498d011ef858636043016edba29d7
Evan Vang <evankvang@gmail.com>
2025-12-17 07:56:34 -0600

    Proper formatting for client with colors. Also let the client restart and input different calculations if desired.

--- CAD-Final/Program.cs
+++ CAD-Final/Program.cs
@@ -1,20 +1,67 @@
 ﻿using CAD_Final;
-using System.Collections.Concurrent;
 
-Console.WriteLine("----!WELCOME TO EVANS LOAN CALCULATOR!----");
+autoLoanCalculator();
+static void autoLoanCalculator()
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("----!WELCOME TO EVANS LOAN CALCULATOR!----");
 
-var carPrice = getPriceOfVehicle();
-var downPayment = getDownPayment();
-var principal = carPrice - downPayment;
+    var carPrice = getPriceOfVehicle();
+    var downPayment = getDownPayment();
+    var principal = carPrice - downPayment;
 
-var loanAmount = getLoanAmount();
-var annualInterestRate = getAnnualInterestRate();
-var numMonths = getNumberOfMonths();
+    var loanAmount = getLoanAmount();
+    var annualInterestRate = getAnnualInterestRate();
+    var numMonths = getNumberOfMonths();
 
-Console.WriteLine($"Monthly Payment: {Math.Round(LoanFormulas.MonthlyPayment(principal, annualInterestRate, numMonths), 2)}");
-Console.WriteLine($"Total Interest Paid {Math.Round(LoanFormulas.TotalPrincipalPaid(principal, annualInterestRate, numMonths), 2)}");
-Console.WriteLine($"Total Principal Paid {Math.Round(LoanFormulas.TotalPrincipalPaid(principal, annualInterestRate, numMonths), 2)}");
-Console.WriteLine($"Total Principal Plus Interest Paid {Math.Round(LoanFormulas.TotalPrincipalPlusInterest(principal, annualInterestRate, numMonths), 2)}");
+    var monthlyPayment = Math.Round(LoanFormulas.MonthlyPayment(principal, annualInterestRate, numMonths), 2);
+    var totalPrincipalPaid = Math.Round(LoanFormulas.TotalPrincipalPaid(principal, annualInterestRate, numMonths), 2);
+    var totalInterestPaid = Math.Round(LoanFormulas.TotalInterestPaid(principal, annualInterestRate, numMonths), 2);
+    var totalPrincipalPlusInterest = Math.Round(LoanFormulas.TotalPrincipalPlusInterest(principal, annualInterestRate, numMonths), 2);
+
+    displayForClient(monthlyPayment, totalPrincipalPaid, totalInterestPaid, totalPrincipalPlusInterest);
+    static void displayForClient(double monthlyPayment, double totalPrincipalPaid, double totalInterestPaid, double totalPrincipalPlusInterest)
+    {
+        try
+        {
+            Console.Write("Monthly Payment: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(monthlyPayment);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\n----------------------------------------------");
+            Console.Write("Total Principal Paid: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(totalPrincipalPaid);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\n----------------------------------------------");
+            Console.Write("Total Interest Paid: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(totalInterestPaid);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\n----------------------------------------------");
+            Console.Write("Total Principal Plus Interest: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(totalPrincipalPlusInterest);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\n----------------------------------------------");
+        }
+        catch
+        {
+            Console.WriteLine("An error occured trying to display ouput. \n Please restart program and try again.");
+        }
+    }
+
+    Console.WriteLine("Type in restart to calculate again");
+    var restartInput = Console.ReadLine();
+
+    if(restartInput == "restart")
+    {
+        autoLoanCalculator();
+    } else
+    {
+        Environment.Exit(0);
+    }
+}
 
 //var test = LoanFormulas.MonthlyPayment(18000, 5.3, 60);
 //var testing = LoanFormulas.TotalInterestPaid(18000, 5.3, 60);
@@ -173,4 +220,4 @@ static double getDownPayment()
 
     throw new Exception("Error in total price of car");
 }
-Console.ReadLine();
+
