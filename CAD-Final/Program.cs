c9d950b782db2cae5e6e7a42d6fa686dace587de
Evan Vang <evankvang@gmail.com>
2025-12-17 09:33:38 -0600

    Fixed a bug that would occur in not returning the correct value after user would type in an invalid input. Also moved user inputs to a seperate class.

--- CAD-Final/Program.cs
+++ CAD-Final/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CAD_Final;
+using System.Transactions;
 
 autoLoanCalculator();
 static void autoLoanCalculator()
@@ -6,13 +7,13 @@ static void autoLoanCalculator()
     Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("----!WELCOME TO EVANS LOAN CALCULATOR!----");
 
-    var carPrice = getPriceOfVehicle();
-    var downPayment = getDownPayment();
+    var carPrice = UserInputFunctions.getPriceOfVehicle();
+    var downPayment = UserInputFunctions.getDownPayment();
     var principal = carPrice - downPayment;
 
-    var loanAmount = getLoanAmount();
-    var annualInterestRate = getAnnualInterestRate();
-    var numMonths = getNumberOfMonths();
+    var loanAmount = UserInputFunctions.getLoanAmount();
+    var annualInterestRate = UserInputFunctions.getAnnualInterestRate();
+    var numMonths = UserInputFunctions.getNumberOfMonths();
 
     var monthlyPayment = Math.Round(LoanFormulas.MonthlyPayment(principal, annualInterestRate, numMonths), 2);
     var totalPrincipalPaid = Math.Round(LoanFormulas.TotalPrincipalPaid(principal, annualInterestRate, numMonths), 2);
@@ -62,162 +63,3 @@ static void autoLoanCalculator()
         Environment.Exit(0);
     }
 }
-
-//var test = LoanFormulas.MonthlyPayment(18000, 5.3, 60);
-//var testing = LoanFormulas.TotalInterestPaid(18000, 5.3, 60);
-//var t = LoanFormulas.TotalPrincipalPaid(18000, 5.3, 60);
-//var e = LoanFormulas.TotalPrincipalPlusInterest(18000, 5.3, 60);
-//Console.WriteLine("Monthly Payment " + test);
-//Console.WriteLine("Total Interest Paid " + testing);
-//Console.WriteLine("Total Principal Paid " + t);
-//Console.WriteLine("Total Principal Plus Interst Paid " + e);
-
-static double getLoanAmount()
-{
-    Console.WriteLine("What is your loan amount?");
-    var loanInput = Console.ReadLine();
-
-    // Now to convert into a number...
-    try
-    {
-        double.TryParse(loanInput, out double loanReturn);
-        loanReturn = Math.Round(loanReturn, 2);
-
-        // First check if its a valid input...
-        if (loanReturn <= 0)
-        {
-            Console.WriteLine("Please try again");
-            // Now lets try again...
-            getLoanAmount();
-        }
-
-        return loanReturn;
-    }
-    catch
-    {
-        // Okay it failed here just try it again;
-        getLoanAmount();
-    }
-
-    throw new Exception("Error in getting loan Amount");
-}
-
-static double getAnnualInterestRate()
-{
-    Console.WriteLine("What is your annual interest rate?");
-    var annualInterestInput = Console.ReadLine();
-
-    // Now to convert into a number...
-    try
-    {
-        double.TryParse(annualInterestInput, out double annualInterestReturn);
-        annualInterestReturn = Math.Round(annualInterestReturn, 2);
-
-        // First check if its a valid input...
-        if (annualInterestReturn <= 0)
-        {
-            Console.WriteLine("Please try again");
-            // Now lets try again...
-            getAnnualInterestRate();
-        }
-
-        return annualInterestReturn;
-    }
-    catch
-    {
-        // Okay it failed here just try it again;
-        getAnnualInterestRate();
-    }
-
-    throw new Exception("Error in getting annual interest rate");
-}
-
-static int getNumberOfMonths()
-{
-    Console.WriteLine("What is the amount of months on the loan?");
-    var monthsInput = Console.ReadLine();
-
-    // Now to convert into a number...
-    try
-    {
-        int.TryParse(monthsInput, out int monthsReturn);
-
-        // First check if its a valid input...
-        if (monthsReturn <= 0)
-        {
-            Console.WriteLine("Please try again");
-            // Now lets try again...
-            getNumberOfMonths();
-        }
-
-        return monthsReturn;
-    }
-    catch
-    {
-        // Okay it failed here just try it again;
-        getNumberOfMonths();
-    }
-
-    throw new Exception("Error in getting loan Amount");
-}
-
-static double getPriceOfVehicle()
-{
-    Console.WriteLine("What is the total price of your vehicle?");
-    var priceInput = Console.ReadLine();
-
-    // Now to convert into a number...
-    try
-    {
-        double.TryParse(priceInput, out double priceReturn);
-        priceReturn = Math.Round(priceReturn, 2);
-
-        // First check if its a valid input...
-        if (priceReturn <= 0)
-        {
-            Console.WriteLine("Please try again");
-            // Now lets try again...
-            getPriceOfVehicle();
-        }
-
-        return priceReturn;
-    }
-    catch
-    {
-        // Okay it failed here just try it again;
-        getPriceOfVehicle();
-    }
-
-    throw new Exception("Error in total price of car");
-}
-
-static double getDownPayment()
-{
-    Console.WriteLine("What is the toal down payment on the vehicle?");
-    var priceInput = Console.ReadLine();
-
-    // Now to convert into a number...
-    try
-    {
-        double.TryParse(priceInput, out double priceReturn);
-        priceReturn = Math.Round(priceReturn, 2);
-
-        // First check if its a valid input...
-        if (priceReturn <= 0)
-        {
-            Console.WriteLine("Please try again");
-            // Now lets try again...
-            getDownPayment();
-        }
-
-        return priceReturn;
-    }
-    catch
-    {
-        // Okay it failed here just try it again;
-        getDownPayment();
-    }
-
-    throw new Exception("Error in total price of car");
-}
-
---
+++ CAD-Final/UserInputFunctions.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAD_Final
+{
+    public static class UserInputFunctions
+    {
+        public static double getLoanAmount()
+        {
+            var validInput = false;
+            var loanReturn = double.MinValue;
+
+            do
+            {
+                Console.WriteLine("What is your loan amount?");
+                var loanInput = Console.ReadLine();
+
+                // Now convert into a number...
+                double.TryParse(loanInput, out loanReturn);
+
+                if (loanReturn <= 0)
+                {
+                    // This will skip to the condition check
+                    // Valid input since it didnt set it true yet should loop back to top.
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input lets try again...");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    continue;
+                }
+                else
+                {
+                    validInput = true;
+                }
+            }
+            while (!validInput);
+
+            return loanReturn;
+        }
+
+        public static double getAnnualInterestRate()
+        {
+            var validInput = false;
+            var interestReturn = double.MinValue;
+
+            do
+            {
+                Console.WriteLine("What is your annual interest rate?");
+                var interestInput = Console.ReadLine();
+
+                // Now convert into a number...
+                double.TryParse(interestInput, out interestReturn);
+
+                if (interestReturn <= 0)
+                {
+                    // This will skip to the condition check
+                    // Valid input since it didnt set it true yet should loop back to top.
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input lets try again...");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    continue;
+                }
+                else
+                {
+                    validInput = true;
+                }
+            }
+            while (!validInput);
+
+            return interestReturn;
+        }
+
+        public static int getNumberOfMonths()
+        {
+            var validInput = false;
+            var monthsReturn = int.MinValue;
+
+            do
+            {
+                Console.WriteLine("What is the amount of months on the loan?");
+                var monthsInput = Console.ReadLine();
+
+                // Now convert into a number...
+                int.TryParse(monthsInput, out monthsReturn);
+
+                if (monthsReturn <= 0)
+                {
+                    // This will skip to the condition check
+                    // Valid input since it didnt set it true yet should loop back to top.
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input lets try again...");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    continue;
+                }
+                else
+                {
+                    validInput = true;
+                }
+            }
+            while (!validInput);
+
+            return monthsReturn;
+        }
+
+        public static double getPriceOfVehicle()
+        {
+            var validInput = false;
+            var priceReturn = double.MinValue;
+
+            do
+            {
+                Console.WriteLine("What is the total price of your vehicle?");
+                var priceInput = Console.ReadLine();
+
+                // Now convert into a number...
+                double.TryParse(priceInput, out priceReturn);
+
+                if (priceReturn <= 0)
+                {
+                    // This will skip to the condition check
+                    // Valid input since it didnt set it true yet should loop back to top.
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input lets try again...");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    continue;
+                }
+                else
+                {
+                    validInput = true;
+                }
+            }
+            while (!validInput);
+
+            return priceReturn;
+        }
+
+        public static double getDownPayment()
+        {
+            var validInput = false;
+            var priceReturn = double.MinValue;
+
+            do
+            {
+                Console.WriteLine("What is the total down payment on your vehicle?");
+                var priceInput = Console.ReadLine();
+
+                // Now convert into a number...
+                double.TryParse(priceInput, out priceReturn);
+
+                if (priceReturn <= 0)
+                {
+                    // This will skip to the condition check
+                    // Valid input since it didnt set it true yet should loop back to top.
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input lets try again...");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    continue;
+                }
+                else
+                {
+                    validInput = true;
+                }
+            }
+            while (!validInput);
+
+            return priceReturn;
+        }
+
+    }
+}
