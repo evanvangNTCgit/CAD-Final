4b0b2a9da1dfd0e305c975762c85812b968ecfa8
Evan Vang <evankvang@gmail.com>
2025-12-16 21:12:13 -0600

    Task 2 getting proper inputs for each variable needed.

--- CAD-Final/Program.cs
+++ CAD-Final/Program.cs
@@ -1,96 +1,97 @@
 ﻿Console.WriteLine("----!WELCOME TO EVANS LOAN CALCULATOR!----");
 
-getLoanAmount();
-getAnnualInterestRate();
-getNumberOfMonths();
+var loanAmount = getLoanAmount();
+var annualInterestRate = getAnnualInterestRate();
+var numMonths = getNumberOfMonths();
+
+Console.WriteLine($"Loan Amonut {loanAmount}, Annual Interest Rate {annualInterestRate}, Number of months {numMonths}");
 
 static double getLoanAmount()
 {
     Console.WriteLine("What is your loan amount?");
     var loanInput = Console.ReadLine();
 
-    return 0;
+    // Now to convert into a number...
+    try
+    {
+        double.TryParse(loanInput, out double loanReturn);
+        loanReturn = Math.Round(loanReturn, 2);
 
-    //// Now to convert into a number...
-    //try
-    //{
-    //    double.TryParse(loanInput, out double loanReturn);
-    //    loanReturn = Math.Round(loanReturn, 2);
-        
-    //    // First check if its a valid input...
-    //    if(loanReturn <= 0)
-    //    {
-    //        // Now lets try again...
-    //        getLoanAmount();
-    //    }
+        // First check if its a valid input...
+        if (loanReturn <= 0)
+        {
+            Console.WriteLine("Please try again");
+            // Now lets try again...
+            getLoanAmount();
+        }
 
-    //    return loanReturn;
-    //} catch
-    //{
-    //    // Okay it failed here just try it again;
-    //    getLoanAmount();
-    //}
+        return loanReturn;
+    }
+    catch
+    {
+        // Okay it failed here just try it again;
+        getLoanAmount();
+    }
 
-    //throw new Exception("Error in getting loan Amount");
+    throw new Exception("Error in getting loan Amount");
 }
 
 static double getAnnualInterestRate()
 {
     Console.WriteLine("What is your annual interest rate?");
-    var loanInput = Console.ReadLine();
+    var annualInterestInput = Console.ReadLine();
 
-    return 0;
+    // Now to convert into a number...
+    try
+    {
+        double.TryParse(annualInterestInput, out double annualInterestReturn);
+        annualInterestReturn = Math.Round(annualInterestReturn, 2);
 
-    //// Now to convert into a number...
-    //try
-    //{
-    //    double.TryParse(loanInput, out double loanReturn);
-    //    loanReturn = Math.Round(loanReturn, 2);
+        // First check if its a valid input...
+        if (annualInterestReturn <= 0)
+        {
+            Console.WriteLine("Please try again");
+            // Now lets try again...
+            getAnnualInterestRate();
+        }
 
-    //    // First check if its a valid input...
-    //    if(loanReturn <= 0)
-    //    {
-    //        // Now lets try again...
-    //        getLoanAmount();
-    //    }
+        return annualInterestReturn;
+    }
+    catch
+    {
+        // Okay it failed here just try it again;
+        getAnnualInterestRate();
+    }
 
-    //    return loanReturn;
-    //} catch
-    //{
-    //    // Okay it failed here just try it again;
-    //    getLoanAmount();
-    //}
-
-    //throw new Exception("Error in getting loan Amount");
+    throw new Exception("Error in getting loan Amount");
 }
 
 static int getNumberOfMonths()
 {
     Console.WriteLine("What is the amount of months on the loan?");
-    var loanInput = Console.ReadLine();
+    var monthsInput = Console.ReadLine();
 
-    return 0;
+    // Now to convert into a number...
+    try
+    {
+        int.TryParse(monthsInput, out int monthsReturn);
 
-    //// Now to convert into a number...
-    //try
-    //{
-    //    double.TryParse(loanInput, out double loanReturn);
-    //    loanReturn = Math.Round(loanReturn, 2);
+        // First check if its a valid input...
+        if (monthsReturn <= 0)
+        {
+            Console.WriteLine("Please try again");
+            // Now lets try again...
+            getNumberOfMonths();
+        }
 
-    //    // First check if its a valid input...
-    //    if(loanReturn <= 0)
-    //    {
-    //        // Now lets try again...
-    //        getLoanAmount();
-    //    }
+        return monthsReturn;
+    }
+    catch
+    {
+        // Okay it failed here just try it again;
+        getNumberOfMonths();
+    }
 
-    //    return loanReturn;
-    //} catch
-    //{
-    //    // Okay it failed here just try it again;
-    //    getLoanAmount();
-    //}
-
-    //throw new Exception("Error in getting loan Amount");
+    throw new Exception("Error in getting loan Amount");
 }
 Console.ReadLine();
