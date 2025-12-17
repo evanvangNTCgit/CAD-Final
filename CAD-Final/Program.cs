10f157c2367a94fc79183cce3968dd20be6b3ed4
Evan Vang <evankvang@gmail.com>
2025-12-16 21:05:37 -0600

    Task 1 Proper texts displayed to console for user input

--- CAD-Final/Program.cs
+++ CAD-Final/Program.cs
@@ -1,1 +1,96 @@
 ﻿Console.WriteLine("----!WELCOME TO EVANS LOAN CALCULATOR!----");
+
+getLoanAmount();
+getAnnualInterestRate();
+getNumberOfMonths();
+
+static double getLoanAmount()
+{
+    Console.WriteLine("What is your loan amount?");
+    var loanInput = Console.ReadLine();
+
+    return 0;
+
+    //// Now to convert into a number...
+    //try
+    //{
+    //    double.TryParse(loanInput, out double loanReturn);
+    //    loanReturn = Math.Round(loanReturn, 2);
+        
+    //    // First check if its a valid input...
+    //    if(loanReturn <= 0)
+    //    {
+    //        // Now lets try again...
+    //        getLoanAmount();
+    //    }
+
+    //    return loanReturn;
+    //} catch
+    //{
+    //    // Okay it failed here just try it again;
+    //    getLoanAmount();
+    //}
+
+    //throw new Exception("Error in getting loan Amount");
+}
+
+static double getAnnualInterestRate()
+{
+    Console.WriteLine("What is your annual interest rate?");
+    var loanInput = Console.ReadLine();
+
+    return 0;
+
+    //// Now to convert into a number...
+    //try
+    //{
+    //    double.TryParse(loanInput, out double loanReturn);
+    //    loanReturn = Math.Round(loanReturn, 2);
+
+    //    // First check if its a valid input...
+    //    if(loanReturn <= 0)
+    //    {
+    //        // Now lets try again...
+    //        getLoanAmount();
+    //    }
+
+    //    return loanReturn;
+    //} catch
+    //{
+    //    // Okay it failed here just try it again;
+    //    getLoanAmount();
+    //}
+
+    //throw new Exception("Error in getting loan Amount");
+}
+
+static int getNumberOfMonths()
+{
+    Console.WriteLine("What is the amount of months on the loan?");
+    var loanInput = Console.ReadLine();
+
+    return 0;
+
+    //// Now to convert into a number...
+    //try
+    //{
+    //    double.TryParse(loanInput, out double loanReturn);
+    //    loanReturn = Math.Round(loanReturn, 2);
+
+    //    // First check if its a valid input...
+    //    if(loanReturn <= 0)
+    //    {
+    //        // Now lets try again...
+    //        getLoanAmount();
+    //    }
+
+    //    return loanReturn;
+    //} catch
+    //{
+    //    // Okay it failed here just try it again;
+    //    getLoanAmount();
+    //}
+
+    //throw new Exception("Error in getting loan Amount");
+}
+Console.ReadLine();
